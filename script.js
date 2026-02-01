911556dbe2e31f5ade43b8892ff113adccf097db
lewi-wajak <lewiwajak@gmail.com>
2026-02-01 22:42:47 +0100

    Initial commit - landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+function subscribe() {
+  const email = document.getElementById("email").value;
+
+  if (email === "") {
+    alert("Please enter your email.");
+  } else {
+    alert("Thank you for joining Healthy Living Hub!");
+  }
+}
